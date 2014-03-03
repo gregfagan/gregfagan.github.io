336d3206ef1890a07133fab642018dd6ea9bb0a3
gregfagan <gregfagan@gmail.com>
2014-03-02 20:44:31 -0800

    New look, getting into CSS. Also set up basic build:
        grunt
        grunt deploy
    
    Importing some other people's css: reset (clears defaults)
    and borderbox fix (from Paul Irish)

---
+++ Gruntfile.js
@@ -0,0 +1,70 @@
+'use strict';
+
+module.exports = function(grunt) {
+    grunt.initConfig({
+        // configuration files
+        pkg: grunt.file.readJSON('package.json'),
+        aws: grunt.file.readJSON('aws.json'),
+
+        watch: {
+            build: {
+                files: '**/*.{js,html,css}',
+                tasks: ['default']
+            }
+        },
+
+        jshint: {
+            build: {
+                src: [
+                    'js/**/*.js',
+                ]
+            },
+        },
+
+        copy: {
+            build: {
+                files: [
+                    {
+                        expand: false,
+                        src: [
+                            'index.html',
+                            'icon.png',
+                            'css/**',
+                            'js/**',
+                        ],
+                        dest: 'build/' 
+                    },
+
+                    {
+                        expand: true,
+                        cwd: 'bower_components/bootstrap/dist/',
+                        src: ['**'],
+                        dest: 'build/'
+                    }
+                ]
+            }
+        },
+
+        s3: {
+            options: {
+                accessKeyId: '<%= aws.key %>',
+                secretAccessKey: '<%= aws.secret %>',
+                bucket: 'gregfagan.com',
+                region: 'us-west-2',
+            },
+
+            deploy: {
+                cwd: 'build',
+                src: '**'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-aws');
+
+    grunt.registerTask('default', ['jshint', 'copy']);
+    grunt.registerTask('deploy', ['s3']);
+};
