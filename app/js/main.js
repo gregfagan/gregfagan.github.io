f6f0bb9762a2870201390f823086af8fac090e01
gregfagan <gregfagan@gmail.com>
2014-04-17 23:15:58 -0700

    Descriptions and blocked out screenshots

--- app/js/main.js
+++ app/js/main.js
@@ -3,18 +3,32 @@ require(['underscore','react-with-addons'], function(_, React) {
         {
             name: "temple3d",
             tech: "C# and Unity3D",
-            collaboration: ["@cjkimberlin"]
+            collaborators: ["@cjkimberlin"],
+            description: "An epic reimagining of the original Ludum Dare game in the third dimension."
         },
         {
             name: "temple2d",
             tech: "JavaScript and Cocos2D-HTML5",
-            collaboration: ["@cjkimberlin"]
+            collaborators: ["@cjkimberlin"],
+            description: "A retro style top down temple exploration adventure built for Ludum Dare 27."
         },
         {
             name: "möbius",
             tech: "ActionScript and FlashPunk",
-            collaboration: ["@cjkimberlin", "@htimswi"]
-        }
+            collaborators: ["@cjkimberlin", "@htimswi"],
+            description: "Minimalist platforming with one button in a mysterious world. A Ludum Dare 26 game jam creation."
+        },
+        {
+            name: "planes of misery",
+            tech: "ActionScript and FlashPunk",
+            collaborators: ["@cjkimberlin", "@htimswi"],
+            description: "Inflict discomfort on the airplane's passengers by seating them next to other obnoxious travelers. Made for Ludum Dare 25."
+        },
+        {
+            name: "because we're hungry, that's why",
+            tech: "C# and XNA",
+            description: "A pair of gorillas take to the city to find some food -- people! Watch out, they are defending themselves with bombs. A single button game for Windows Phone."
+        },
     ];
 
     var Collaborator = React.createClass({
@@ -26,12 +40,12 @@ require(['underscore','react-with-addons'], function(_, React) {
 
     var CollaboratorList = React.createClass({
         render: function() {
-            var collaborators = ["with: "];
+            var collaborators = ["with "];
             var that = this;
-            this.props.collaboration.forEach(function(person, index) {
+            this.props.collaborators.forEach(function(person, index) {
                 collaborators.push(Collaborator({handle: person}));
-                if (index != that.props.collaboration.length - 1) {
-                    collaborators.push(", ");
+                if (index != that.props.collaborators.length - 1) {
+                    collaborators.push(" and ");
                 }
             });
             return React.DOM.span(null, collaborators);
@@ -40,15 +54,22 @@ require(['underscore','react-with-addons'], function(_, React) {
 
     var Project = React.createClass({
         render: function() {
-            return React.DOM.div(
-                {className: "project"}, 
-                [
-                    React.DOM.img({src: this.props.screenshot}),
-                    React.DOM.h1(null, this.props.name),
-                    React.DOM.h2(null, this.props.tech),
-                    CollaboratorList({collaboration: this.props.collaboration})
-                ]
-            );
+            imgBlock = React.DOM.img({src: this.props.screenshot});
+
+            var children = [
+                React.DOM.h1(null, this.props.name),
+                React.DOM.h2(null, this.props.tech)
+            ];
+
+            if (this.props.collaborators) {
+                children.push(CollaboratorList({collaborators: this.props.collaborators}));
+            }
+
+            children.push(React.DOM.p(null, this.props.description));
+
+            var descriptionBlock = React.DOM.div(null, children);
+
+            return React.DOM.div({className: "project"}, [imgBlock, descriptionBlock]);
         }
     });
 
