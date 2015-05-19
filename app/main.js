39abe3515f70cc568ff6de3e976f54ae28a30843
gregfagan <gregfagan@gmail.com>
2015-05-19 00:59:10 -0700

    Remove default styling

--- app/main.js
+++ app/main.js
@@ -1,7 +1,8 @@
 var _ = require('lodash');
 var React = require('react/addons');
-var data = require('data.json');
-
 var App = require('App');
+require('normalize.css');
+
+var data = require('data.json');
 
 React.render(<App data={data}/>, document.getElementById('app'));
