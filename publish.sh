118c2a48272b42adade0fe1c0848f9a9b94b57f9
Greg Fagan <gregfagan@gmail.com>
2017-04-18 13:57:55 -0500

    Update publish script to exclude JS

--- publish.sh
+++ publish.sh
@@ -4,5 +4,6 @@ git init
 git remote add origin https://github.com/gregfagan/gregfagan.github.io.git
 git checkout --orphan master
 git add --all
+git reset -- bundle.js
 git commit -m "Publishing"
 git push origin master -f
